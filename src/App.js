e1d6d2c1141c127f1549a6ce5788dc69a1153424
PB220-DEVLOP <pratikbhoyar2002@gmail.com>
2023-04-26 13:16:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    
+    <><h1>
+      welcome to the page 
+    </h1>
+    <p>thi is the page in the following syntax and wets begin the following format</p>
+    </>
+  )
+}
+
+export default App;
